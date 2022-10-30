870ffa3022652b6bf982a2302610526e300cd7f1
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-10-30 18:04:27 +0600

    Spread Operator and Variable Hoisting More Explanation

--- 02_javascriptFundamental/05_SpreadOperator.js
+++ 02_javascriptFundamental/05_SpreadOperator.js
@@ -1,6 +1,14 @@
 let poorCountry = ["Bangladesh", "Srilangka"];
 let richCountry = ["America", "Canada","Italy"];
-
 richCountry = [...poorCountry,"America","Canada","Italy"];
+// ["Bangladesh", "Srilangka","America", "Canada","Italy"]
+console.log("Rich Country :" + richCountry);
 
-console.log(richCountry);
+let Pcountry = ["Bangladesh", "Srilangka"];
+let Rcountry = ["America", "Canada","Italy"];
+Pcountry.push(Rcountry);
+// ["Bangladesh", "Srilangka",["America", "Canada","Italy"]]
+console.log("P Country : " + Pcountry);
+
+console.log("Rich Country[2] :" + richCountry[2]); // "America"
+console.log("P country[2] " + Pcountry[2]); //["America", "Canada","Italy"]
--- 02_javascriptFundamental/10_VariableHoisting.js
+++ 02_javascriptFundamental/10_VariableHoisting.js
@@ -1,3 +1,6 @@
 name = "Ruman Hossain";
 console.log(name);
 var name;
+//Variable Call Value Assign and Call first but Declaration Later.
+//var only works for variable hoisting (CAUSE GLOBAL SCOPE)
+//let const will not work (CAUSE BLOCK SCOPE for Block Scope Declaration First)
