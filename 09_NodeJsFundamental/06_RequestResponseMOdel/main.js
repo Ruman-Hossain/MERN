47ef065281be35a65c62c022b018e5b2421dfb63
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-11-07 18:58:17 +0600

    Request Response Model

---
+++ 09_NodeJsFundamental/06_RequestResponseMOdel/main.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var server = http.createServer(function(req,res){
+    if(req.url ==  "/"){
+        res.writeHead(200,{'Content-Type' : 'text/html'});
+        res.write('<h1>This is Home Page</h1>');
+        res.end();
+    }else if(req.url == '/about'){
+        res.writeHead(200,{'Content-Type' : 'text/html'});
+        res.write('<h1>This is About Page</h1>');
+        res.end();
+    }else if(req.url == '/contact'){
+        res.writeHead(200,{'Content-Type' : 'text/html'});
+        res.write('<h1>This is Contact Page</h1>');
+        res.end();
+    }
+});
+
+server.listen(5050);
+console.log("Server Run Success!");
