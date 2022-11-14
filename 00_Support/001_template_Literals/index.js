c0dac5c91ec5eacb7657c2011f91a6fb4ecf4630
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-11-15 02:03:14 +0600

    Template Literals vs Normal String Literals

---
+++ 00_Support/001_template_Literals/index.js
@@ -0,0 +1,8 @@
+//Normal String Literals
+console.log("We Love Javasript"); 
+let instituteName = "Lead Educare";
+console.log(instituteName);
+
+//Template Literals       ||  Instead of Single or Double Quote You have to Use Back Tick Symbol
+// Inside Back Tick You can write string and variable values as well for variables just us ${variableName}
+console.log(`We are learning at ${instituteName}`);
---
+++ 00_Support/test.js
@@ -0,0 +1,1 @@
+console.log("Hello This is a test file");
