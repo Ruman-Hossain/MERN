7e3868a1e8d1752f874adc5d9621b9c9c7a084f5
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-11-15 02:17:13 +0600

    Template Literals and Normal String Literals

--- 00_Support/001_template_Literals/index.js
+++ 00_Support/001_template_Literals/index.js
@@ -6,3 +6,13 @@ console.log(instituteName);
 //Template Literals       ||  Instead of Single or Double Quote You have to Use Back Tick Symbol
 // Inside Back Tick You can write string and variable values as well for variables just us ${variableName}
 console.log(`We are learning at ${instituteName}`);
+
+function sum(a,b){
+    return a+b;
+}
+var doSum = true;
+for(let x = 5,y = 20 ; x <= 20 ; x += 5, y -= 5){
+    let result = `Sum of ${x} + ${y} = ${x+y}`;
+    console.log(doSum ? result : "Skipped");
+    doSum ? doSum = false : doSum = true;
+}
