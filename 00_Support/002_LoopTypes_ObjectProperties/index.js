183c5870b9242852cc77c9eca0c291adb469448b
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-11-15 11:55:59 +0600

    Folder Renamed to Loop Types Object Properties

---
+++ 00_Support/002_LoopTypes_ObjectProperties/index.js
@@ -0,0 +1,87 @@
+console.log('LOOP----> FOR OF') 
+var numbers = [45,87,89,56,32,51,25,188,41,25,98];
+for(let number of numbers){
+    console.log(number);
+}
+/* OUTPUT
+LOOP----> FOR OF
+45
+87
+89
+56
+32
+51
+25
+188
+41
+25
+98
+*/
+
+console.log('LOOP-------> FOR IN'); 
+const len = numbers.length;
+for(let i = 0; i < len ; i++){
+    console.log(numbers[i]);
+}
+/* OUTPUT
+LOOP-------> FOR IN
+45
+87
+89
+56
+32
+51
+25
+188
+41
+25
+98
+*/
+console.log('LOOP-------> FOR IN  || CONTINUE | BREAK'); 
+for(let i = 0; i < len ; i++){
+    if(numbers[i]<=30)
+        continue;
+    else if(numbers[i]==188)
+        break;
+    else
+        console.log(numbers[i]);
+}
+/*OUTPUT: 
+45 
+87 
+89 
+56 
+32 
+51 
+*/
+const sakib={
+    name:"Sakib al hassan",
+    age:35,
+    isActive:true,
+    isAllRounder:true,
+    country:"Bangladesh",
+    scores:[35,89,65]
+}
+sakib['isBatsman']=true
+console.log(sakib)
+/*OUTPUT
+{
+  name: 'Sakib al hassan',
+  age: 35,
+  isActive: true,
+  isAllRounder: true,
+  country: 'Bangladesh',
+  scores: [ 35, 89, 65 ],
+  isBatsman: true
+}
+*/
+console.log(sakib.country);  //Bangladesh
+console.log(`The cricketer name is ${sakib.name}, his age is ${sakib.age}, country is ${sakib['country']}`)
+//The cricketer name is Sakib al hassan, his age is 35, country is Bangladesh
+console.log(Object.keys(sakib));
+console.log(sakib.keys());
+console.log(Object.values(sakib));
+
+for(let element in sakib){
+    console.log(sakib[element]);
+}
