af51bba9e9388eadb2910594aecfa83f18424821
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-11-17 15:41:25 +0600

    Config hidden

--- 016_NodeJS_Fun_MongoDB_Crud/01_connect_with_mongoDB_cluster/index.js
+++ 016_NodeJS_Fun_MongoDB_Crud/01_connect_with_mongoDB_cluster/index.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
-var URL = "mongodb+srv://ruman:ruman@cluster0.tnpja.mongodb.net/school?retryWrites=true&w=majority";
+var URL = "mongodb+srv://<username>:<password>@cluster0.tnpja.mongodb.net/school?retryWrites=true&w=majority";
 var config = {useUnifiedTopology:true};
 MongoClient.connect(URL,config,function(error){
     if(error){
