836a4aab2c42a167e1ded4aeef1a525d5cede6db
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-11-17 14:30:19 +0600

    Delete One Data from mongoDB

---
+++ 016_NodeJS_Fun_MongoDB_Crud/03_delete-data_nodejs_mongodb/deleteOne.js
@@ -0,0 +1,25 @@
+var MongoClient = require('mongodb').MongoClient;
+var URL = "mongodb+srv://ruman:ruman@cluster0.tnpja.mongodb.net/?retryWrites=true&w=majority";
+var config = {useUnifiedTopology:true};
+MongoClient.connect(URL,config,function(error,myMongoClient){
+    if(error){
+        console.log("Connection Failed");
+    }else{
+        console.log("Connection Successful");
+        deleteData(myMongoClient);
+    }
+});
+
+function deleteData(myMongoClient){
+    var myDatabase = myMongoClient.db('school');
+    var myCollection = myDatabase.collection('students');
+    var deleteItem = {roll:'04'};
+    myCollection.deleteOne(deleteItem,function(error){
+        if(error){
+            console.log("Data Delete Failed");
+        }else{
+            console.log("Data Delete Successfull");
+        }
+    });
+    
+}
