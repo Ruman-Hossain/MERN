6b4e41e4f3d561dc7fdf14fdc1efca7b9d45c74b
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-11-19 19:07:28 +0600

    Insert Update(one|Many) Find(One|Many) Delete(One|Many) Create Collection Delete collection

--- 016_NodeJS_Fun_MongoDB_Crud/05_Insert_Delete_OneorMany_Find_OneOrMany_Update_CreateCollection/index.js
+++ 016_NodeJS_Fun_MongoDB_Crud/05_Insert_Delete_OneorMany_Find_OneOrMany_Update_CreateCollection/index.js
@@ -1,19 +1,15 @@
 var MongoClient=require('mongodb').MongoClient;
-
 var URL="mongodb+srv://ruman:ruman@cluster0.tnpja.mongodb.net/?retryWrites=true&w=majority";
-
 var config= { useUnifiedTopology: true };
-
 MongoClient.connect(URL,config,function (error,myMongoClient) {
     if(error){
         console.log("Connection Fail")
-    }
-    else{
+    }else{
         console.log("Connection Success");
-        InsertData(myMongoClient);
+        // InsertData(myMongoClient);
         // DeleteOneItem(myMongoClient);
-        // DeleleAllItem(myMongoClient);
-        // FindOneWithoutCondition(MongoClient);
+        DeleleAllItem(myMongoClient);
+        // FindOneWithoutCondition(myMongoClient);
         // FindOneWithCondition(myMongoClient);
         // FindAllData(myMongoClient);
         // FindAllDataByProjection(myMongoClient);
@@ -23,15 +19,14 @@ MongoClient.connect(URL,config,function (error,myMongoClient) {
         // UpdateMyData(myMongoClient);
         // CreateMyCollection(myMongoClient);
         // DeleteMyCollection(myMongoClient);
-
     }
 });
 
-
+//INSERT DATA WORKS FINE
 function InsertData(myMongoClient){
    var MyDataBase= myMongoClient.db("school");
    var MyCollection= MyDataBase.collection('students');
-   var MyData={name:"Ruman",Roll:"02",Class:"Ten",City:"Rangpur"};
+   var MyData={name:"Hossain",Roll:"03",Class:"Ten",City:"Rangpur"};
    MyCollection.insertOne(MyData,function (error) {
        if(error){
            console.log("Data Insert Fail");
@@ -42,11 +37,11 @@ function InsertData(myMongoClient){
    });
 }
 
-
+//DELETE ONE DATA WORKS FINE
 function DeleteOneItem(myMongoClient) {
     var MyDataBase= myMongoClient.db("school");
     var MyCollection= MyDataBase.collection('students');
-    var DeleteItem={Roll:"02"}
+    var DeleteItem={Roll:"05"}
     MyCollection.deleteOne(DeleteItem,function (error) {
         if(error){
             console.log("Data Delete Fail");
@@ -56,38 +51,41 @@ function DeleteOneItem(myMongoClient) {
         }
     });;
 }
-
+//DELETE ALL ITEM WORKS FINE
 function DeleleAllItem(myMongoClient) {
     var MyDataBase= myMongoClient.db("school");
     var MyCollection= MyDataBase.collection('students');
     MyCollection.deleteMany(function (error,ResultObj){
         if(error){
-            console.log("Delete Fail");
+            console.log("Delete Failed");
         }
         else {
-            console.log(ResultObj.result.n +" Item Deleted");
+            console.log(ResultObj);
         }
     });
 }
-
+//FIND ONE WITHOUT CONDITION WORKS FINE
 function FindOneWithoutCondition(myMongoClient) {
-    var MyDataBase= myMongoClient.db("school");
-    var MyCollection= MyDataBase.collection('students');
-    var FindObj={}
-    MyCollection.findOne(FindObj,function (error,result) {
+    var database = myMongoClient.db('school');
+    var myCollection = database.collection('students');
+    var queryObj = {};
+    myCollection.find(queryObj).toArray(function(error,result){
+    // myCollection.find(queryObj).limit(20).toArray(function(error,result){
         console.log(result);
     });
 }
 
+//FIND ONE WITH CONDITION WORKS FINE
 function FindOneWithCondition(myMongoClient) {
     var MyDataBase= myMongoClient.db("school");
     var MyCollection= MyDataBase.collection('students');
-    var FindObj={Roll: "05"}
+    var FindObj={Roll: "02"}
     MyCollection.findOne(FindObj,function (error,result) {
         console.log(result);
     });
 }
 
+//FIND ALL DATA WORKS FINE
 function FindAllData(myMongoClient) {
     var MyDataBase= myMongoClient.db("school");
     var MyCollection= MyDataBase.collection('students');
@@ -96,74 +94,85 @@ function FindAllData(myMongoClient) {
     });
 }
 
+//FIND ALL DATA BY PROJECTION WORKS FINE
 function FindAllDataByProjection(myMongoClient) {
     var MyDataBase= myMongoClient.db("school");
     var MyCollection= MyDataBase.collection('students');
 
     var ItemObj={}
     // var ItemProjection={projection:{Class:"",Roll:""}}  Wrong In Video
-    var ItemProjection={projection:{Class:1,Roll:1}} // This is correct
+    var ItemProjection={projection:{Class:1,Roll:1}} // This is correct || Will Display Class and Roll Column
 
     MyCollection.find(ItemObj,ItemProjection).toArray(function (error,result) {
         console.log(result)
     });
 }
 
+//FIND ALL DATA USING QUERY WORKS FINE
 function FindAllDataByQuery(myMongoClient) {
     var MyDataBase= myMongoClient.db("school");
     var MyCollection= MyDataBase.collection('students');
 
-    var Query={Roll :"01",City: "Rangpur"}
+    var Query={name: "Zahid",City: "Rangpur"}
+    // var Query = {name:{$in:['Ruman','Zahid','Anwar Zahid']}}; //If want to search Multiple Value
 
     MyCollection.find(Query).toArray(function (error,result) {
         console.log(result)
     });
 }
-
+//FIND ALL DATA BY LIMIT WORKS FINE
 function FindAllDataByLimit(myMongoClient) {
     var MyDataBase= myMongoClient.db("school");
     var MyCollection= MyDataBase.collection('students');
-
-    MyCollection.find().limit(20).toArray(function (error,result) {
-        console.log(result)
+    var Query = {name:{$in:['Ruman','Zahid','Anwar Zahid']}};
+    MyCollection.find(Query).limit(2).toArray(function (error,result) {
+        console.log(result);
     });
 }
-
+//FIND ALL DATA AFTER SORTING WORKS FINE
 function FindAllDataBySort(myMongoClient) {
     var MyDataBase= myMongoClient.db("school");
     var MyCollection= MyDataBase.collection('students');
-
-    var SortPattern={Class :-1}
+    // ASC : 1  DESC: -1
+    var SortPattern={Roll : -1}
 
     MyCollection.find().sort(SortPattern).toArray(function (error,result) {
-        console.log(result)
+        console.log(result);
     });
 }
-
+//UPDATE MY DATA ON DATABASE WORKS FINE
 function UpdateMyData(myMongoClient){
     var MyDataBase= myMongoClient.db("school");
     var MyCollection= MyDataBase.collection('students');
 
-    var MyQuery={Roll:"4"};
-    var MyNewValues={$set:{name:"Anwar Jahid Ruman",City:"Rangpur"}}
+    var MyQuery={Roll:"10"};
+    var MyNewValues={$set:{name:"Anwar Jahid Ruman Hossain",City:"Rangpur"}}
 
     MyCollection.updateOne(MyQuery,MyNewValues,function (error,result) {
         console.log(result);
     });
 }
 
+//CREATE COLLECTION WORKS FINE
 function CreateMyCollection(myMongoClient){
     var MyDataBase= myMongoClient.db("school");
     MyDataBase.createCollection("teachers",function (error,result) {
-        console.log(result);
+        if(error){
+            console.log("Collection Already Exists or There's a Problem!");
+        }else{
+            console.log(result);
+        }
     });
 }
-
+//DELETE COLLECTION WORKS FINE
 function DeleteMyCollection(myMongoClient){
     var MyDataBase= myMongoClient.db("school");
 
     MyDataBase.dropCollection("teachers",function (error,result) {
-        console.log(result);
+        if(error)
+            console.log("Collection Doesn't Exists to Delete");
+        else
+            console.log(result);
     });
 
 }
