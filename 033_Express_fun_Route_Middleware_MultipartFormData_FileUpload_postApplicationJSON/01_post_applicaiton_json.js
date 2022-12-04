681d39f10a4028f2ccb3ab4a9100fa4f7d4d6876
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-12-04 17:08:47 +0600

    Post JSON Application | Multipart Form Data

--- 033_Express_fun_Route_Middleware_MultipartFormData_FileUpload_postApplicationJSON/01_post_applicaiton_json.js
+++ 033_Express_fun_Route_Middleware_MultipartFormData_FileUpload_postApplicationJSON/01_post_applicaiton_json.js
@@ -1,18 +1,30 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const { json } = require('body-parser');
 const app = express();
-
+const bodyParser = require('body-parser');
+//We will send name and City using postman Body Section | JSON FORMAT and will catch to display it as response
 app.use(bodyParser.json());
 
-//We will send name and City using postman Body Section | JSON FORMAT and will catch to display it as response
-app.post('/',(req, res)=>{
+app.post('/json',(req, res)=>{
     let jsonData = req.body;
     //let jsonString = JSON.stringify(jsonData);
     let name = jsonData['name'];
     let city = jsonData['city'];
     res.send(name+" "+city); //Returns Ruman Hossain Rangpur
     //res.send(jsonString);  //Returns JSON FORMAT Output: {"name":"Ruman Hossain", "city":"Rangpur"}
+});
+
+//Multipart-Form Data | MULTER 
+//Input in Postman Body Section|| Form-data : name: Ruman Hossain, city: Rangpur, Country:Bangladesh
+var multer = require('multer');
+var multer = multer();
+
+app.use(multer.array());
+app.use(express.static('public'));
+
+app.post('/multipart',(req,res)=>{
+    let jsonData = req.body;
+    let jsonString = JSON.stringify(jsonData);
+    res.send(jsonString); // Output: {"name":"Ruman Hossain","city":"Rangpur","country":"Bangladesh"}
 });
 
 app.listen(5050,()=>{
