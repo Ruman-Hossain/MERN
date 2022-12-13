ebcc4f9bc89092c23a5c2c89ad429d75df0d3b44
Ruman-Hossain <ruman.cse.brur@gmail.com>
2022-12-13 19:10:13 +0600

    Undefined Route Handling

--- 040_Express_fun_API_Route_Middleware_EnvironemntVariable/app.js
+++ 040_Express_fun_API_Route_Middleware_EnvironemntVariable/app.js
@@ -4,4 +4,13 @@ const app = new express();
 app.use('/api/v1',router);
 
 
+//Undefined Route
+app.use('*',(req,res)=>{
+    res.status(404).json({
+        "status":"fail",
+        "data":"Not Found"
+    })
+});
+
+
 module.exports=app;
