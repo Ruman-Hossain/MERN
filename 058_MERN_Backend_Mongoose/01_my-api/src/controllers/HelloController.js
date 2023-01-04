11dd74cf0c95659f2ce5e22effff1043814d884c
Md Ruman Hossain <ruman.cse.brur@gmail.com>
2023-01-04 15:13:37 +0600

    Deleted Dummy File Folders

--- 058_MERN_Backend_Mongoose/01_my-api/src/controllers/HelloController.js
+++
@@ -1,6 +0,0 @@
-exports.HelloGet=(req,res)=>{
-    res.status(203).json({status:'Success',data:"Hello GET"});
-};
-exports.HelloPost=(req,res)=>{
-    res.status(201).json({status:"Success",data:"Hello POST"});
-};
