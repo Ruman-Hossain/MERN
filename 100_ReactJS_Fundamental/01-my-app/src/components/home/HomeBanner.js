b742a5d7fdea73f85a4415cef568f6b76fa6544d
Md Ruman Hossain <ruman.cse.brur@gmail.com>
2023-02-18 13:56:11 +0600

    Functional Component useEffect Hooks

--- 100_ReactJS_Fundamental/01-my-app/src/components/home/HomeBanner.js
+++ 100_ReactJS_Fundamental/01-my-app/src/components/home/HomeBanner.js
@@ -1,6 +1,8 @@
-import React, { useRef, /*useState*/ } from 'react';
+import axios from 'axios';
+import React, { useEffect,useRef, useState } from 'react';
 
 const HomeBanner = (props) => {
+    // // ============= USE STATE AND PROPS STARTS================= // 
     // const [color, setColor] = useState('Red');
     // const newBlue=()=>{
     //     setColor('Blue');
@@ -8,23 +10,49 @@ const HomeBanner = (props) => {
     // const newGreen=()=>{
     //     setColor('Green');
     // }
-    const userName = useRef();
-    const password = useRef();
-    const subMitForm=()=>{
-        const name = userName.current.value;
-        const pass = password.current.value;
-        console.log(`Username : ${name} Password : ${pass}`);
-    }
+    // // ============= USE STATE AND PROPS ENDS================= // 
+
+    // // ============= USE REF STARTS================= // 
+    // const userName = useRef();
+    // const password = useRef();
+    // const subMitForm=()=>{
+    //     const name = userName.current.value;
+    //     const pass = password.current.value;
+    //     console.log(`Username : ${name} Password : ${pass}`);
+    // }
+    // // ============= USE REF STARTS================= // 
+
+    // //  =========== USE EFFECT STARTS ================= //
+    const [todo,setTodo] = useState([]);
+    useEffect(()=>{
+        axios.get('https://jsonplaceholder.typicode.com/todos').then((res)=>{
+            setTimeout(()=>{
+                setTodo(res.data);
+            },1000);
+            
+        }).catch((error)=>{
+
+        });
+    });
+
     return (
+        // ============= USE STATE AND PROPS STARTS ============ //
         // <div>
         //     <h1>{color}</h1>
         //     <button onClick={newBlue}>Change To Blue</button>
         //     <button onClick={newGreen}>Change To Green</button>
         // </div>
+        // ============= USE STATE AND PROPS ENDS ============ //
+        // ============= USE REF STARTS ============ //
+        // <div>
+        //     <input type="text" ref={userName} placeholder="Enter Your User Name"/>
+        //     <input type="text" ref={password} placeholder="Enter Your User Name"/>
+        //     <button onClick={subMitForm}>Submit</button>
+        // </div>
+        // ============= USE REF ENDS ============ //
+
         <div>
-            <input type="text" ref={userName} placeholder="Enter Your User Name"/>
-            <input type="text" ref={password} placeholder="Enter Your User Name"/>
-            <button onClick={subMitForm}>Submit</button>
+            {JSON.stringify(todo)}
         </div>
     );
 };
