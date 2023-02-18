75ba24f37e16d84b2842fce90fd36599fb21ff47
Md Ruman Hossain <ruman.cse.brur@gmail.com>
2023-02-18 17:06:56 +0600

    Class Components Initial Commit

--- 102_Start-ecommerce-frontend-part/03_class-components/src/components/home/HomeBanner.js
+++ 102_Start-ecommerce-frontend-part/03_class-components/src/components/home/HomeBanner.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 
 export default class HomeBanner extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    constructor(){
+        super();
+    }
+    render() {
+        return (
+        <div>
+            
+        </div>
+        )
+    }
 }
--- 102_Start-ecommerce-frontend-part/03_class-components/src/pages/HomePage.js
+++ 102_Start-ecommerce-frontend-part/03_class-components/src/pages/HomePage.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 
 export default class HomePage extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+    constructor(){
+        super();
+    }
+    render() {
+        return (
+        <div>
+            
+        </div>
+        )
+    }
 }
