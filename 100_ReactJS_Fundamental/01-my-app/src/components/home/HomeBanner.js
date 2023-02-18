3343f6311ce509494329a4e703f34b2249254733
Md Ruman Hossain <ruman.cse.brur@gmail.com>
2023-02-18 13:39:08 +0600

    React Hook useRef

--- 100_ReactJS_Fundamental/01-my-app/src/components/home/HomeBanner.js
+++ 100_ReactJS_Fundamental/01-my-app/src/components/home/HomeBanner.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useRef, /*useState*/ } from 'react';
 
 const HomeBanner = (props) => {
+    // const [color, setColor] = useState('Red');
+    // const newBlue=()=>{
+    //     setColor('Blue');
+    // };
+    // const newGreen=()=>{
+    //     setColor('Green');
+    // }
+    const userName = useRef();
+    const password = useRef();
+    const subMitForm=()=>{
+        const name = userName.current.value;
+        const pass = password.current.value;
+        console.log(`Username : ${name} Password : ${pass}`);
+    }
     return (
+        // <div>
+        //     <h1>{color}</h1>
+        //     <button onClick={newBlue}>Change To Blue</button>
+        //     <button onClick={newGreen}>Change To Green</button>
+        // </div>
         <div>
-            <h1>This is {props.title.name}, and I'm {props.title.age}'s old</h1>
+            <input type="text" ref={userName} placeholder="Enter Your User Name"/>
+            <input type="text" ref={password} placeholder="Enter Your User Name"/>
+            <button onClick={subMitForm}>Submit</button>
         </div>
     );
 };
--- 100_ReactJS_Fundamental/01-my-app/src/pages/HomePage.js
+++ 100_ReactJS_Fundamental/01-my-app/src/pages/HomePage.js
@@ -4,7 +4,7 @@ import HomeBanner from '../components/home/HomeBanner';
 const HomePage = () => {
     return (
         <div>
-            <HomeBanner title={{name:"Ruman Hossain",age:26}}/>
+            <HomeBanner />
         </div>
     );
 };
