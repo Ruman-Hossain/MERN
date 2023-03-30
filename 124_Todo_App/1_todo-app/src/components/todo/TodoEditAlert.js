bfd3ed6ff7794d3efaf30c5f53baf9edcacfe8ae
Md Ruman Hossain <ruman.cse.brur@gmail.com>
2023-03-30 21:22:39 +0600

    Todo Update SweetAlert Added

--- 124_Todo_App/1_todo-app/src/components/todo/TodoEditAlert.js
+++ 124_Todo_App/1_todo-app/src/components/todo/TodoEditAlert.js
@@ -1,6 +1,6 @@
 import Swal from "sweetalert2";
 import store from "../../redux/store/store";
-import {EditTodo} from "../../redux/state/todo/todoSlice";
+import {EditTodo, RemoveTodo} from "../../redux/state/todo/todoSlice";
 
 
 
@@ -12,7 +12,21 @@ export function TodoEditAlert(i,item){
         inputValue:item,
         inputValidator:(value)=>{
             if(value){
-                store.dispatch(EditTodo({index:i,task:value}))
+                //store.dispatch(EditTodo({index:i,task:value}))
+                Swal.fire({
+                    title: 'Do you want to save the changes?',
+                    showDenyButton: true,
+                    showCancelButton: true,
+                    confirmButtonText: 'Update',
+                    denyButtonText: `Don't Update`,
+                }).then((result) => {
+                    /* Read more about isConfirmed, isDenied below */
+                    if (result.isConfirmed) {
+                        Swal.fire('Updated!', '', 'success')
+                    } else if (result.isDenied) {
+                        Swal.fire('Changes are not saved', '', 'info')
+                    }
+                })
             }
         }
     })
